8ef53e922b26f756aa927826d63c5e53279e894a
GAURAV DNYANESH MAHAJAN <108111718+Gaurav-22-11@users.noreply.github.com>
2023-02-17 17:49:32 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,7 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  //cout << "Hello from PES1UG20CS150-GAURAV\n ";
-  //cout << "Webhook !!!\n ";
+  cout << "Hello from PES1UG20CS150-GAURAV\n ";
+  cout << "Webhook !!!\n ";
   return 0;
 }
